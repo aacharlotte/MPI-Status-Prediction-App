581c513e31153be1685037cd502692923c5cfe3e
Charlotte Arinaitwe <charlotte.arinaitwe@gmail.com>
2025-10-01 15:55:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load pipeline (model with processor)
-pipeline = joblib.load("tr_pipeline.pkl")
+pipeline = joblib.load("tr_pipeline(5).pkl")
 
 st.title("Random Forest Multidimensional Poverty Status Prediction App")
 
